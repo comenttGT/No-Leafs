6659f369bc4f3dee4e0c487cc81fe27ba7f53b3d
comenttGT <100043986+comenttGT@users.noreply.github.com>
2024-10-29 16:35:08 +0100

    Add files via upload

---
+++ NoLeaves/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace NoLeaves
+{
+    [BepInPlugin("ComenttGT", "NoLeaves", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Start() => GorillaTagger.OnPlayerSpawned(OnSpawn);
+
+        private void OnSpawn()
+        {
+            GameObject[] allObj = FindObjectsOfType<GameObject>();
+            for(int i = 0; i < allObj.Length; i++)
+            {
+                if (allObj[i].name == "fallleaves (combined by EdMeshCombinerSceneProcessor)")
+                    allObj[i].SetActive(false);
+                else
+                    continue;
+            }
+        }
+    }
+}
